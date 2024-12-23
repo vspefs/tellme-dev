8894c66d4128778372626c06543d38c0fed98c5a
vspefs <vspefs@protonmail.com>
2024-12-23 20:36:26 +0800

    use `addModule()` right

--- build.zig
+++ build.zig
@@ -20,7 +20,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    b.addModule("tellme", .{
+    _ = b.addModule("tellme", .{
         .optimize = optimize,
         .target = target,
         .root_source_file = b.path("src/root.zig"),
